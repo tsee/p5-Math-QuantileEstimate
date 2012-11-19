e97bf5ae6ec7fa6c6fe92a700b5b063901f258d9
Steffen Mueller <smueller@cpan.org>
2012-11-19 20:35:38 +0100

    Test O(1) variant of  summary_quantile_query

--- ctest/100c_summary.c
+++ ctest/100c_summary.c
@@ -5,13 +5,26 @@
 
 #include "mytap.h"
 
+void basic_summary_tests();
+
 int
 main ()
+{
+    basic_summary_tests();
+
+    ok_m(1, "alive");
+    done_testing();
+    return 0;
+}
+
+void
+basic_summary_tests()
 {
     const qe_uint b = 10;
     const qe_uint n = 5;
     const qe_float v[] = {12.3, -1., 1., 4., 2.};
     qe_tuple_t *tuples;
+    qe_tuple_t *t;
     qe_uint i, j;
 
     /* Build up a very basic summary with index = rank-1 */
@@ -31,16 +44,26 @@ main ()
 
     for (i = 1; i <= n; ++i) {
         j = summary_rank_binsearch(s, i);
-        /* Assert index = rank-1 */
-        is_int_m(j, i-1, "Base level summary: rank-1 is same as index");
-
-        /* Assert rank relationships */
-        ok_m(s->tuples[j].lower_rank <= s->tuples[j].upper_rank, "Rank range for item is sane");
-        if (j != 0) {
-            ok_m(s->tuples[j].value >= s->tuples[j-1].value, "Value monotonically increasing");
-            ok_m(s->tuples[j].lower_rank > s->tuples[j-1].upper_rank, "Rank strictly monotonically increasing");
+#define CHECK_RANK_RESULT(msg) \
+        /* Assert index = rank-1 */ \
+        is_int_m(j, i-1, "Base level summary: rank-1 is same as index (" msg ")"); \
+            \
+        /* Assert rank relationships */ \
+        ok_m(s->tuples[j].lower_rank <= s->tuples[j].upper_rank, "Rank range for item is sane"); \
+        if (j != 0) { \
+            ok_m(s->tuples[j].value >= s->tuples[j-1].value, "Value monotonically increasing"); \
+            ok_m(s->tuples[j].lower_rank > s->tuples[j-1].upper_rank, "Rank strictly monotonically increasing"); \
         }
+        CHECK_RANK_RESULT("summary_rank_binsearch")
+
+        /* Test O(1) variant of summary_quantile_query */
+        t = summary_quantile_query(s, i);
+        ok_m(t != NULL, "summary_quantile_query yields valid result");
+        is_int_m(t->lower_rank, t->upper_rank, "Upper/lower ranks are identical");
+        j = t->lower_rank-1;
+        CHECK_RANK_RESULT("summary_quantile_query")
     }
+
 
     /* Manually construct summary with actual rank ranges for testing */
     {
@@ -51,6 +74,9 @@ main ()
             tuples[i].lower_rank = lower_ranks[i];
             tuples[i].upper_rank = upper_ranks[i];
         }
+        /* Set the compressed flag since rank ranges imply
+         * that it's the output of compress */
+        QE_SUMMARY_SET_FLAG(s, QE_SUMMARY_F_COMPRESSED);
 
         /* Yes, this is dumb. Patches to generate from the above rank tables welcome. */
         j = summary_rank_binsearch(s, 1);
@@ -86,8 +112,7 @@ main ()
         is_int_m(tuples[j].upper_rank, upper_ranks[4], "Base level summary: upper rank according to rank table (8)");
     }
 
+    
+
     summary_free(s);
-    ok_m(1, "alive");
-    done_testing();
-    return 0;
 }
